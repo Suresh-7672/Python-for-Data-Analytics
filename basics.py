6654246806cfb0b71ff9b47da46264d2b027581f
suresh thalluri <147021347+Suresh-7672@users.noreply.github.com>
2026-06-02 14:59:58 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,21 @@
+
+
+#candies=10
+#for i in range (candies):
+#    if candies-1==5:
+ #       print("only 5 condies left")
+ #       continue
+#print("giving a candy to a frnd")g
+
+#n=int(input())
+#i=1
+#while i<=n:
+ #   print(i)
+ #   i+=1
+
+
+n=int(input())
+sum=0
+for i in range(1,n+1):
+    sum+=1
+    print(sum)

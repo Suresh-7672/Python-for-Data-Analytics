00914a9941421a51681bc4722b3750431f73fb71
suresh thalluri <147021347+Suresh-7672@users.noreply.github.com>
2026-06-05 21:21:56 +0530

    Refactor basics.py with various functionalities

--- basics.py
+++ basics.py
@@ -19,3 +19,44 @@ sum=0
 for i in range(1,n+1):
     sum+=1
     print(sum)
+
+# grade calculation 
+marks = int(input("Enter marks : "))
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+elif marks >= 60:
+    print("Grade D")
+else:
+    print("E")
+
+#largest of 3 numbers
+a = int(input())
+b = int(input())
+c = int(input())
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+else :
+    print(c)
+
+#profit , loss
+cp = int(input())
+sp = int(input())
+if sp > cp:
+    print("profit", sp - cp)
+elif sp < cp:
+    print("loss", sp - cp)
+else:
+    print("no profit no loss")
+
+# leap year
+year = int(input())
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("leap year")
+else:
+    print("not a lea year")

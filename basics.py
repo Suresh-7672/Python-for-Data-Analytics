cefbbd5049f9c591d7882ffe61d629a463249347
suresh thalluri <147021347+Suresh-7672@users.noreply.github.com>
2026-06-11 23:23:57 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1,17 +1,17 @@
 
 
-#candies=10
-#for i in range (candies):
-#    if candies-1==5:
- #       print("only 5 condies left")
- #       continue
-#print("giving a candy to a frnd")g
+candies=10
+for i in range (candies):
+    if candies-1==5:
+        print("only 5 condies left")
+        continue
+print("giving a candy to a frnd")g
 
-#n=int(input())
-#i=1
-#while i<=n:
- #   print(i)
- #   i+=1
+n=int(input())
+i=1
+while i<=n:
+    print(i)
+    i+=1
 
 
 n=int(input())
